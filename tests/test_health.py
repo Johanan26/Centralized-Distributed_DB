f03d18ef8f6b373ad438ecfe4c30a550dcc4e465
Johanan26 <120645993+Johanan26@users.noreply.github.com>
2025-11-06 13:14:07 +0000

    added a bunch of new code and test cases, needs to be optimised later

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,5 +1,5 @@
-#Tests to see if the health function is running properly
-def test_health(client):
- r = client.get("/health")
- assert r.status_code == 200
- assert r.json() == {"status": "ok"}
+# #Tests to see if the health function is running properly
+# def test_health(client):
+#  r = client.get("/health")
+#  assert r.status_code == 200
+#  assert r.json() == {"status": "ok"}
