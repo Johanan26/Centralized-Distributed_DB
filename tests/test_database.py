d5f9067cac97f4ec01e2d4909887272095d63564
Johanan26 <120645993+Johanan26@users.noreply.github.com>
2025-11-07 23:39:46 +0000

    Fixed error in pytest

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,4 +1,29 @@
 import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+
+from app.main import app, get_db
+from app.models import Base
+
+TEST_DB_URL = "sqlite+pysqlite:///:memory:"
+engine = create_engine(TEST_DB_URL, connect_args={"check_same_thread": False}, poolclass=StaticPool,)
+TestingSessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+Base.metadata.create_all(bind=engine)
+
+@pytest.fixture
+def client():
+  def override_get_db():
+    db = TestingSessionLocal()
+    try:
+      yield db
+    finally:
+      db.close()
+  app.dependency_overrides[get_db] = override_get_db
+  with TestClient(app) as c:
+# hand the client to the test
+    yield c
 
 def user_payload(name="User",email="user@atu.ie", age=20, sid="S9000001"):
     return{"name": name, "email": email, "age": age, "student_id":sid}
